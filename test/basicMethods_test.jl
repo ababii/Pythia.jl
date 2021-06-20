3740690d5dbe4a2f2d629ecceff4125ab2399020
Arvind Ganesh <arvind.cganesh@gmail.com>
2021-06-20 16:27:00 -0500

    Removed dependency, added comments.

--- test/basicMethods_test.jl
+++ test/basicMethods_test.jl
@@ -6,3 +6,4 @@ mdl = MeanForecast(y, h = 5, level = [80, 95])
 mdlResult = fit(mdl)
 
 @test mdlResult.fittedvalues = [1.0, 2.0, 3.0, 4.0, 5.0, 3.0, 3.0, 3.0, 3.0, 3.0]
+### Will add tests for prediction intervals in later commits.
