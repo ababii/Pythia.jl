2712d171ad2b55c871faa771bd27065b06cfd565
Arvind Ganesh <arvind.cganesh@gmail.com>
2021-06-26 14:11:21 -0500

    Added Naive Model, removed DS_Store from gitignore

--- test/basicMethods_test.jl
+++ test/basicMethods_test.jl
@@ -1,9 +1,19 @@
 using Pythia
 using Test
-
+ 
+### Mean Forecast Tests ###
 y = [1.0, 2.0, 3.0, 4.0, 5.0]
 mdl = MeanForecast(y, h = 5, level = [80, 95])
 mdlResult = fit(mdl)
 
-@test mdlResult.fittedvalues = [1.0, 2.0, 3.0, 4.0, 5.0, 3.0, 3.0, 3.0, 3.0, 3.0]
+@test mdlResult.fittedvalues == [1.0, 2.0, 3.0, 4.0, 5.0, 3.0, 3.0, 3.0, 3.0, 3.0]
+### Will add tests for prediction intervals in later commits.
+
+
+### Naive Forecast Tests ###
+y = [1.0, 2.0, 3.0, 4.0, 5.0]
+mdl = NaiveForecast(y, h = 5, level = [80, 95])
+mdlResult = fit(mdl)
+
+@test mdlResult.fittedvalues == [1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
 ### Will add tests for prediction intervals in later commits.
